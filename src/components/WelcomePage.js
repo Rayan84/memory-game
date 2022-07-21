70e2e260dde8845d26d78c88a4ad2b63374a0a9a
Rayan84 <rayan_84@live.com>
2022-07-21 21:35:33 +0200

    Remove loading animation page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,15 +1,48 @@
-const WelcomePage = () => {
+import { useEffect } from "react";
+import { useState } from "react";
+
+const LoadingAnimation = (props) => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    if(counter < 100){
+      console.log('starting...')
+    };
+    let timeOut = 0;
+    const interval = setInterval(() => {
+    
+      console.log('Interval running...');
+      if(timeOut < 100){
+        setCounter(counter => counter + 1)
+        timeOut = timeOut + 1;
+        console.log(timeOut);
+
+      }else {
+        clearInterval(interval);
+      }
+      
+    } ,35);
+    return () => clearInterval(interval);
+  }, []);
+
   return(
     <div>
+    {
+      counter < 100 ? `${counter} %` : <div>
+
+      
       <h1>Memory Game</h1>
       <p>Choose a level</p>
       <ul>
-        <li>Easy</li>
+        <li className='cursor-pointer'>Easy</li>
         <li>Meduim</li>
         <li>Hard</li>
       </ul>
     </div>
-  )
-};
+    }
+      
+    </div>
 
-export default WelcomePage;
+  );
+}
+
+export default LoadingAnimation;
