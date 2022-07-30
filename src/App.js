d76e33b9050ff3bb369868f6e46c61df36ffd44e
Rayan84 <rayan_84@live.com>
2022-07-30 11:16:36 +0200

    Remove unneeded code lines from App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import React from 'react';
 import './App.css';
 import RouterComponent from './components/RouterComponent';
-// import WelcomePage from './components/WelcomePage';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   // Link,
-//   Route,
-//   // NavLink,
-// } from 'react-router-dom';
-// import Easy from './components/Easy';
-// import Medium from './components/Medium';
-// import Hard from './components/Hard';
-// import Nav from './components/Nav';
+
 function App() {
-  // const currentPath = window.location.pathname;
-
-
   return (
     <div className="App">
       <header className="App-header">
- 
-
         <div>
-      {/* <Router>
-      <Routes>
-        <Route path="/medium" element={<Medium />} />
-        <Route path="/hard" element={<Hard />} />
-        <Route path="/easy" element={<Easy />} />
-        <Route path="/" element={<div><WelcomePage /> <Nav /></div>} />
-      </Routes>
-    </Router>   */}
-    <RouterComponent />
-    </div>  
+          <RouterComponent />
+        </div>  
       </header>
     </div>
   );
